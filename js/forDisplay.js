f8464896e8479eca29e01a4011fb5c25891d4a45
Jon Zielenkievicz <jonzielen@gmail.com>
2017-06-25 17:27:48 -0400

    add styles

--- js/forDisplay.js
+++ js/forDisplay.js
@@ -1,16 +1,18 @@
 var forDisplay = function(data, elem) {
   var hostElem = document.getElementById(elem),
-      template = `<h4>Pages: ${data.pages} - ${data.times}</h4>`;
+      template = `<h4>Pages: ${data.pages} - ${data.times}</h4><div class="flex">`;
 
   data.data.forEach(function(set) {
-    template = template + `<h5>Items: ${set.length}</h5>`;
+    template = template + `<div class="flex-item"><h5>Items: ${set.length}</h5>`;
 
     template = template + `<ul>`;
     set.forEach(function(elem) {
       template = template + `<li>${elem.title}  ${elem.cardFormat}</li>`;
     });
-    template = template + `</ul>`;
+    template = template + `</ul></div>`;
   });
+
+  template = template + `</div>`;
 
   hostElem.innerHTML = template;
 };
